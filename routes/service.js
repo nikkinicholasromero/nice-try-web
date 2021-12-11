30711212d4ff8f5bda5b4d6707377c842600ba3d
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2021-12-11 08:07:23 -0500

    Registration endpoint

--- routes/service.js
+++ routes/service.js
@@ -1,12 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var fs = require('fs');
+var sourceFolder = 'C:\\Users\\nikki\\Desktop\\files\\';
 
-router.all('/register', function(req, res, next) {
-  // TODO
-  // Return error if username is already present in DB
-  // Insert new user_information
-  // Create new folder in target folder
-  res.json({status: "Registration is not yet supported"});
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'nice_try'
+});
+
+router.post('/register', function(req, res, next) {
+  var query = `SELECT * FROM USER_INFORMATION WHERE USERNAME = "${req.body.username}"`;
+  connection.query(query, function (err, rows, fields) {
+    if (err) throw err;
+
+    if (rows.length > 0) {
+      res.json({status: "FAILED", message: "Username already used. "});
+    } else {
+
+      var insert = `INSERT INTO USER_INFORMATION (USERNAME, PASSWORD) VALUES ("${req.body.username}", "${req.body.password}")`;
+      connection.query(insert, function (err, rows, fields) {
+        if (err) throw err;
+        
+        fs.mkdirSync(`${sourceFolder}${req.body.username}`, { recursive: true });
+        res.json({status: "SUCCESS", message: "Registration successful. "});
+      });
+    }
+  });
 });
 
 router.all('/authenticate', function(req, res, next) {
