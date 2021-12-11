283ef41e2a4b853b313608a27db7814158f28f07
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2021-12-11 07:26:26 -0500

    Send email OTP

--- routes/login.js
+++ routes/login.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
+var nodemailer = require('nodemailer');
 
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'root',
   database: 'nice_try'
+});
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: '{{EMAIL_ADDRESS}}',
+    pass: '{{EMAIL_PASSWORD}}'
+  }
 });
 
 router.get('/', function(req, res, next) {
@@ -26,8 +35,21 @@ router.post('/', function(req, res, next) {
       connection.query(update, function (err, rows, fields) {
         if (err) throw err;
 
-        // TODO : Send email with OTP
-        console.log("Generated OTP is : " + otp);
+        var mailOptions = {
+          from: '{{EMAIL_ADDRESS}}',
+          to: req.body.username,
+          subject: 'Nice Try OTP',
+          text: `Your OTP is ${otp}`
+        };
+
+        transporter.sendMail(mailOptions, function(error, info){
+          if (error) {
+            console.log(error);
+          } else {
+            console.log('Email sent: ' + info.response);
+          }
+        });
+        
         res.render('otp', { username: req.body.username });
       });
       
