c5f201085cafea9382f4cfd5f38b15441bb54945
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2021-12-11 05:46:32 -0500

    Implemented download functionality

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var testFolder = 'C:\\Users\\nikki\\Desktop\\files';
+var zip = require('zip-a-folder');
 
 router.get('/', function(req, res, next) {
   if (req.session.user === undefined) {
@@ -7,6 +9,12 @@ router.get('/', function(req, res, next) {
   } else {
     res.render('index');
   }
+});
+
+router.post('/', function(req, res, next) {
+  zip.zip(testFolder + "\\" + req.session.user.name, testFolder + "\\" + req.session.user.name + ".zip").then(() => {
+    res.download(testFolder + "\\" + req.session.user.name + ".zip");
+  });
 });
 
 module.exports = router;
