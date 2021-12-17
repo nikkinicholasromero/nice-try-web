5a9daa8f21dd2e654c30da74ac978643297e943e
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2021-12-17 13:52:04 -0500

    Decrypts file pre-download

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var path = require('path');
 var sourceFolder = 'C:\\files\\';
+var tempFolder = 'C:\\temp\\';
 var zip = require('zip-a-folder');
 const { exec } = require("child_process");
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   if (req.session.user === undefined) {
     res.redirect('/login');
   } else {
@@ -12,23 +15,56 @@ router.get('/', function(req, res, next) {
   }
 });
 
-router.post('/', function(req, res, next) {
-  // copy files to a new temp folder
-  // decrypt temp folder
-  // zip temp folder
-  // delete temo folder
-  // download temp folder
-  // delete zip
+router.post('/', function (req, res, next) {
+  function copyFileSync(source, target) {
+    var targetFile = target;
 
-  // exec(`java Decrypt ${sourceFolder}${req.session.user.name}`, (error, stdout, stderr) => {
-  //   console.log(`stdout: ${stdout}`);
-  //   var targetFolder = `${sourceFolder}${req.session.user.name}`;
-  //   var targetDestination = `${targetFolder}.zip`;
-  
-  //   zip.zip(targetFolder, targetDestination).then(() => {
-  //     res.download(targetDestination);
-  //   });
-  // });
+    // If target is a directory, a new file with the same name will be created
+    if (fs.existsSync(target)) {
+      if (fs.lstatSync(target).isDirectory()) {
+        targetFile = path.join(target, path.basename(source));
+      }
+    }
+
+    fs.writeFileSync(targetFile, fs.readFileSync(source));
+  }
+
+  function copyFolderRecursiveSync(source, target) {
+    var files = [];
+
+    // Check if folder needs to be created or integrated
+    var targetFolder = path.join(target, path.basename(source));
+    if (!fs.existsSync(targetFolder)) {
+      fs.mkdirSync(targetFolder);
+    }
+
+    // Copy
+    if (fs.lstatSync(source).isDirectory()) {
+      files = fs.readdirSync(source);
+      files.forEach(function (file) {
+        var curSource = path.join(source, file);
+        if (fs.lstatSync(curSource).isDirectory()) {
+          copyFolderRecursiveSync(curSource, targetFolder);
+        } else {
+          copyFileSync(curSource, targetFolder);
+        }
+      });
+    }
+  }
+
+  fs.mkdirSync(`${tempFolder}${req.session.user.name}`, { recursive: true });
+  copyFolderRecursiveSync(`${sourceFolder}${req.session.user.name}`, `${tempFolder}`);
+  exec(`java Decrypt ${tempFolder}${req.session.user.name}\\`, (error, stdout, stderr) => {
+    var targetFolder = `${tempFolder}${req.session.user.name}`;
+    var targetDestination = `${targetFolder}.zip`;
+
+    zip.zip(targetFolder, targetDestination).then(() => {
+      res.download(targetDestination, "files.zip", function() {
+        fs.rm(targetFolder, { recursive: true }, () => {});
+        fs.unlinkSync(targetDestination);
+      });
+    });
+  });
 });
 
 module.exports = router;
